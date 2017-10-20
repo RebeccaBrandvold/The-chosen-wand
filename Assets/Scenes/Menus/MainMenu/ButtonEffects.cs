90f64e84bd85b23893aba47e282cb4167e5b6879
RebeccaBrandvold <31205894+RebeccaBrandvold@users.noreply.github.com>
2017-10-20 20:11:35 +0200

    Added more buttons and scripts
    Scripts are waiting for the Scenes correctly places atm.
    
    Tried out the simple animation that i was talking about. Think this could work now. 8)

---
+++ Assets/Scenes/Menus/MainMenu/ButtonEffects.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ButtonEffects : MonoBehaviour {
+
+    public Button StartGameButton;
+    public Button LoadGameButton;
+    public Button SettingsGameButton;
+    public Button ExitGameButton;
+
+    public void startPress()
+    {
+        SceneManager.LoadScene(1/*SceneNumber*/);
+    }
+
+    public void LoadSavePress()
+    {
+        SceneManager.LoadScene(1/*SceneNumber*/);
+    }
+
+    public void SettingsPress()
+    {
+        SceneManager.LoadScene(1/*SceneNumber*/);
+    }
+
+    public void ExitPress()
+    {
+        SceneManager.LoadScene(1/*SceneNumber*/);
+    }
+
+
+}
