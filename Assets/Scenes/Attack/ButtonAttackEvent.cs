cdea6ffe812ac1a4beb74aa0e0302496b43147a6
RebeccaBrandvold <31205894+RebeccaBrandvold@users.noreply.github.com>
2017-10-29 02:45:58 +0100

    Buttons In the attack now dissapears when you press them.
    So far what I need to get done on that is
    Find the pattern to press, then kill animal code.
    Find the wrong patterns pressed to kill the player after 3 tries.
    Give the player a timer maybe.

---
+++ Assets/Scenes/Attack/ButtonAttackEvent.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ButtonAttackEvent : MonoBehaviour {
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+    public GameObject button4;
+    public GameObject button5;
+
+    public float timeCount = 3f;
+    private bool AnimalKilled = false;
+    private bool AnimalAttacks = false;
+  
+    
+
+  
+	void Start () {
+        //  button1.SetActive(false);//to make buttons dissapear and reappear FALSE REMOVES BUTTON
+        //  button2.SetActive(true);//to make buttons dissapear and reappear TRUE SHOWS BUTTON
+
+        /*button1 = GetComponent<Button>();
+        button1.onClick.AddListener(()=> HideButton());*/
+        //button1 = GameObject.Find("Hit");
+    }
+
+    public void Button1Press()
+    {
+        // button1 = GetComponent<Button>();
+        button1.SetActive(false);
+    }
+    public void Button2Press()
+    {
+        button2.SetActive(false);
+    }
+    public void Button3Press()
+    {
+        button3.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update () {
+       // button1.SetActive(true);//to make buttons dissapear and reappear
+       // button2.SetActive(false);//to make buttons dissapear and reappear 
+    }
+ 
+
+}
+/*public GameObject button1;
+public GameObject button2;
+public GameObject button3;
+private bool AnimalKilled = false;
+private bool AnimalAttacks = false;*/
+
+/*What I want :
+    1 : Gjøre sånn at knappene popper opp på skjermen. V
+    2 : Gjøre sånn at knappene forsvinner når man har trykket på dem.
+    3 : Gjøre sånn at hvis knappene har blitt trykket på i en spesiel rekke følge dreper man dyret.           
+    4 : Gjøre sånn at hvis knappene som ikke er riktig rekkefølge tar bort en av 3 sjanser.
+    5 : Kanskje legge til tid på de
+    6 : Gjøre sånn at man dør når 'livene' er brukt opp
+    */
