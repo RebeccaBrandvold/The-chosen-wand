f47fb583346d354587dda2c67391f5745fd4b205
EmmaHolmPedersen <EmmaHolmPedersen@users.noreply.github.com>
2017-11-29 17:11:57 +0100

    Working on health!
    Health kinda implemented. It works on player and enemy, but have a hard time making the player able to heal the companion. You press too much and array is out of index although it shouldn't be which causes it to do nothing.... I think

---
+++ Assets/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject thisObj;
+    public float speed = 3;
+    public Transform target;
+
+    private Vector3 pos;
+    private bool notmoving = false;
+
+    //private Rigidbody2D rb;
+
+    void Start()
+    {
+        //thisObj = this.GetComponent<GameObject>();
+        pos = transform.position;
+        //rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+
+        if (!notmoving)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+        }
+    }
+
+    private IEnumerator OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Companion"))
+        {
+            notmoving = true;
+            transform.position = pos;
+            // thisObj.SetActive(false);
+            yield return new WaitForSeconds(3);
+            //thisObj.SetActive(true);
+            notmoving = false;
+        }
+    }
+}
