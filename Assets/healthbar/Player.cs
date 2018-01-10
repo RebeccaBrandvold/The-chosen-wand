17470f7740dce29cba8d5cb747ae0c6bd049564c
RebeccaBrandvold <31205894+RebeccaBrandvold@users.noreply.github.com>
2018-01-10 13:06:00 +0100

    Health goes down on all 3 animals when pressed Q goes up with Space button
    Now I need to make the healing only be when the player is within a trigger collider. And only heals one at a time.  Not two. And that the health of the animals goes down over time by itself.

---
+++ Assets/healthbar/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    [SerializeField]
+    private Stat health;
+
+
+    private void Awake() //
+    {
+        health.Initialized();
+    }
+	// Update is called once per frame
+	void Update () {
+	
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            health.CurrentVal -= 10; //reduces current value with 10
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            health.CurrentVal += 5; //reduces current value with 10
+        }
+    }
+}
---
+++ Assets/healthbar/Stat.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class Stat {
+
+    //So that the health can tell the bar that its monitoring
+    [SerializeField]
+    private HealthBarScript bar;
+    [SerializeField]
+    private float maxVal;
+    [SerializeField]
+    private float currentVal; //Current value of this stat. 
+
+    public float CurrentVal
+    {
+        get
+        {
+            return currentVal;
+        }
+
+        set
+        {           //shoudln't be able to go belowmax and low
+            this.currentVal = Mathf.Clamp(value, 0, MaxVal);
+            bar.Value = currentVal;
+        }
+    }
+
+    public float MaxVal
+    {
+        get
+        {
+            return maxVal;
+        }
+
+        set
+        {
+            this.maxVal = value;
+            bar.MaxValue = maxVal;
+        }
+    }
+    public void Initialized() //seeting the maxx value
+    {
+        this.MaxVal = maxVal; //updates
+        this.CurrentVal = currentVal; //updates
+    }
+}
