d1aa91bd9c30a72729f0d5a768e92e2487632730
RebeccaBrandvold <31205894+RebeccaBrandvold@users.noreply.github.com>
2018-01-10 16:54:19 +0100

    Everything is hell
    Colliders doesn't want to collide.

--- Assets/healthbar/Player.cs
+++ Assets/healthbar/Player.cs
@@ -1,25 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
     [SerializeField]
     private Stat health;
+    public float decreasePerMinute;
 
+    public bool witchHealing ;
 
     private void Awake() //
     {
         health.Initialized();
     }
-	// Update is called once per frame
-	void Update () {
-	
-        if(Input.GetKeyDown(KeyCode.Q))
-        {
-            health.CurrentVal -= 10; //reduces current value with 10
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        //Trying to get the health to decrease over time. 
+        health.CurrentVal -= Time.deltaTime * decreasePerMinute / 60f;
+
+        //if (Input.GetKeyDown(KeyCode.Q))
+        //{
+            //health.CurrentVal -= 10; //reduces current value with 10
+        //}
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            health.CurrentVal += 5; //reduces current value with 10
+            if(witchHealing)
+            {
+                health.CurrentVal += 100; //reduces current value with 10
+           
+            }
+            
+           
+            //health.CurrentVal += 100; //reduces current value with 10
+            //Debug.Log("bo" + health.CurrentVal);
+        }
+    }
+
+    //detect
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //if (transform.parent && transform.parent.gameObject.name + "heal" + other.gameObject.name) ;
+
+        if (other.gameObject.CompareTag("animal"))
+        {
+            witchHealing = true;
+        }
+        else
+        {
+            witchHealing = false;
         }
     }
 }
