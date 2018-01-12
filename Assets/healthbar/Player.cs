7586e5c0d7044266c0299bd7e628ec281ac2594c
EmmaHolmPedersen <EmmaHolmPedersen@users.noreply.github.com>
2018-01-12 15:09:32 +0100

    Fixed healing
    I hope

--- Assets/healthbar/Player.cs
+++ Assets/healthbar/Player.cs
@@ -19,34 +19,39 @@ public class Player : MonoBehaviour
         //Trying to get the health to decrease over time. 
         health.CurrentVal -= Time.deltaTime * decreasePerMinute / 60f;
 
-        //if (Input.GetKeyDown(KeyCode.Q))
-        //{
-            //health.CurrentVal -= 10; //reduces current value with 10
-        //}
-        if (Input.GetKeyDown(KeyCode.Space))
+        // if (Input.GetKeyDown(KeyCode.Q))
+        // {
+        //   health.CurrentVal += 10; //reduces current value with 10
+        // }
+        if (Input.GetKeyDown(KeyCode.Space) && witchHealing)
         {
-            if(witchHealing)
-            {
-                health.CurrentVal += 100; //reduces current value with 10
-           
-            }
-            
-           
+
+            health.CurrentVal += 10; //reduces current value with 10
+
+
             //health.CurrentVal += 100; //reduces current value with 10
-            //Debug.Log("bo" + health.CurrentVal);
+            Debug.Log("bo" + health.CurrentVal);
         }
+
     }
 
     //detect
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         //if (transform.parent && transform.parent.gameObject.name + "heal" + other.gameObject.name) ;
 
-        if (other.gameObject.CompareTag("animal"))
+        if (other.gameObject.CompareTag("Player"))
         {
             witchHealing = true;
         }
-        else
+     //  else
+     //  {
+     //      witchHealing = false;
+     //  }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
         {
             witchHealing = false;
         }
